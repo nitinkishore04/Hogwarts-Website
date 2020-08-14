a2403eddfffc88f4742cdb598a063d4f3f3bb218
Nitin Kishore <nitin.kishore04@gmail.com>
2020-08-14 20:53:32 +0530

    Hogwarts Website

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+
+
+window.sr = ScrollReveal();
+
+sr.reveal('h2.display-2',{
+duration:2000,
+origin:'left',
+distance:'30px'
+});
+
+sr.reveal('.header a.btn',{
+duration:2000,
+origin:'bottom',
+distance:'30px'
+});
+
+// Features Animation
+var d = 0;
+for(i=0;i<3;i++){
+    let spec = document.getElementsByClassName('spec')[i];
+    sr.reveal(spec,{
+        duration:2000,
+        origin:'bottom',
+        distance:'30px',
+        delay:d
+        });
+    d += 250;
+}
+
+// Animation Products Images
+sr.reveal('.products img',{
+    duration:3000,
+    origin:'bottom',
+    distance:'30px',
+    });
+
+// Animation last-button
+sr.reveal('.last-sec a',{
+    duration:2000,
+    origin:'bottom',
+    distance:'30px',
+    delay:d
+    });
